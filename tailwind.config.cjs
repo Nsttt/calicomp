1c8167a04b162e2d7faca114ee667a1fff8b0f49
Nestor <elnestinatorHD@gmail.com>
2022-01-14 18:36:06 +0100

    Add tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./src/**/*.{html,svelte,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        'cali-blue': '#264BFF',
+      },
+      fontFamily: {
+        calicomp: ['CyberpunkWaifus'],
+      },
+    },
+  },
+  plugins: [],
+};
