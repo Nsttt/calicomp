f9c6aa1591037abf566f49e1164364cc1886b197
Nestor <elnestinatorHD@gmail.com>
2022-01-16 20:37:25 +0100

    Move layout to __layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,13 @@
-<script>
+<script lang="ts">
+  import Header from '$lib/header.svelte';
+
   import '../app.css';
 </script>
 
-<slot />
+<Header />
+<main class="flex flex-col bg-black h-screen">
+    <slot />
+  <div class="block bg-cali-blue p-1 mt-auto ml-auto border-white border-t-4 border-l-4">
+    <img class="w-28" src="calicomp-logo.png" alt="Calicomp Logo" />
+  </div>
+</main>
