48ca88ef8a55cc183b7e62a2624c3545847260bc
Nestor <elnestinatorHD@gmail.com>
2022-01-16 20:37:31 +0100

    Cleanup main page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,24 +1,9 @@
-<header class="flex w-full text-black">
-  <ul class="flex flex-row">
-    <li class="pr-1 hover:bg-black hover:text-white">By Name</li>
-    <li class="px-1 hover:bg-black hover:text-white">By Flavor</li>
-    <li class="px-1 hover:bg-black hover:text-white">By Type</li>
-  </ul>
-  <ul class="ml-auto">
-    <li class="hover:bg-black hover:text-white">About</li>
-  </ul>
-</header>
-<main class="flex flex-col bg-black h-screen">
-  <div class="flex flex-col items-center justify-center m-auto">
-    <img class="w-36" src="btc.png" alt="B.T.C Recipe Book" />
-    <p class="text-xl my-4">Recipe Book</p>
-    <div class="bg-white h-1 w-full my-3" />
-    <div class="text-center max-w-xs">
-      <h2 class="text-3xl">Welcome back.</h2>
-      <h3 class="text-xl">Use the top navigation bar to start browsing.</h3>
-    </div>
+<div class="flex flex-col items-center justify-center m-auto">
+  <img class="w-36" src="btc.png" alt="B.T.C Recipe Book" />
+  <p class="text-xl my-4">Recipe Book</p>
+  <div class="bg-white h-1 w-full my-3" />
+  <div class="text-center max-w-xs">
+    <h2 class="text-3xl">Welcome back.</h2>
+    <h3 class="text-xl">Use the top navigation bar to start browsing.</h3>
   </div>
-  <div class="block bg-cali-blue p-1 mt-auto ml-auto border-white border-t-4 border-l-4">
-    <img class="w-28" src="calicomp-logo.png" alt="Calicomp Logo" />
-  </div>
-</main>
+</div>
