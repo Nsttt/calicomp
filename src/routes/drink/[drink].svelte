6bd8521ca00d8e8dc29003608e022dda63aa3bae
Nestor <elnestinatorHD@gmail.com>
2022-01-26 21:19:24 +0100

    Dynamic drink page WIP

---
+++ src/routes/drink/[drink].svelte
@@ -0,0 +1,9 @@
+<script lang="ts" context="module">
+  import { drink } from '$store/drinkStore';
+</script>
+
+<h1 class="p-2 text-3xl">{$drink.name}</h1>
+<h2 class="p-2 text-base max-w-xs">{$drink.description}</h2>
+<h2 class="p-2 text-base max-w-xs">{$drink.flavor}</h2>
+<h2 class="p-2 text-base max-w-xs">{$drink.type}</h2>
+<h2 class="p-2 text-base max-w-xs">{$drink.techniques}</h2>
