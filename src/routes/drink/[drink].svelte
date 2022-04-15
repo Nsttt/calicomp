7449e0bbfce0bc31ff6e9e029853333388c0bd4b
Nestor <elnestinatorHD@gmail.com>
2022-04-15 20:28:37 +0200

    Reboot project to react-native

--- src/routes/drink/[drink].svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts" context="module">
-  import { drink } from '$store/drinkStore';
-</script>
-
-<h1 class="p-2 text-3xl">{$drink.name}</h1>
-<h2 class="p-2 text-base max-w-xs">{$drink.description}</h2>
-<h2 class="p-2 text-base max-w-xs">{$drink.flavor}</h2>
-<h2 class="p-2 text-base max-w-xs">{$drink.type}</h2>
-<h2 class="p-2 text-base max-w-xs">{$drink.techniques}</h2>
--- src/routes/index.svelte
+++
@@ -1,9 +0,0 @@
-<div class="flex flex-col items-center justify-center m-auto">
-  <img class="w-36" src="btc.png" alt="B.T.C Recipe Book" />
-  <p class="text-xl my-4">Recipe Book</p>
-  <div class="bg-white h-1 w-full my-3" />
-  <div class="text-center max-w-xs">
-    <h2 class="text-3xl">Welcome back.</h2>
-    <h3 class="text-xl">Use the top navigation bar to start browsing.</h3>
-  </div>
-</div>
